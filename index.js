b9362edad45525ddd094a83e1f6193d86d2ae4bf
luiscabezasdev <luiscabezas25@hotmail.com>
2022-06-30 12:51:24 -0500

    Add API

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-fetch("./data.json")
+var oddOne = document.querySelector("#oddBar");
+console.log(oddOne);
+const guardar = document.querySelector("#oddBar");
+console.log(guardar)
+
+fetch("./data.json", {
+   method: "GET",
+   headers: {"Content-type": "application/json;charset=UTF-8"}
+ })
 .then(response => {
-   return response.json();
+   const res = response.json();
+   return res;
 })
 .then(jsondata => console.log(jsondata));
+
+
+
+// const API_URL = "./data.json";
+
+/** async function loadData() {
+   const response = await fetch('./data.json');
+   const data = response.json();
+   return data;
+}
+
+loadData(); **/
+
